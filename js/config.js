44f5038b9f96f9f452722c2ae5d7891d34397297
Shahab uddin <114737338+shahab3051@users.noreply.github.com>
2026-09-16 10:50:44 +0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -46,7 +46,7 @@
 const APP_CONFIG = {
   USE_MOCK_DATA: false,
 
-  GOOGLE_SHEETS_API_URL: "https://script.google.com/macros/s/REPLACE_WITH_YOUR_DEPLOYMENT_ID/exec",
+  GOOGLE_SHEETS_API_URL: "https://script.google.com/macros/s/AKfycbxqLFrwd5i5KGMlKEq507Q09MBi73ea0vEShBezar9EcSa1PuZvJVz448AyTcN_x-8okQ/exec",
   BIOMETRIC_MIDDLEWARE_URL: "https://REPLACE_WITH_YOUR_LOCAL_MIDDLEWARE/api",
 
   COMPANY_NAME: "Simply Connect",
